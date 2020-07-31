ee3b9f2a54a6afeeeb1b9cd001dfba1f4e1273ff
sudhanshu <sudhanshu@sudhanshu>
2020-07-31 20:18:20 +0530

    Add place api code commited

--- API_Demo_Project/src/demo/Basics.java
+++ API_Demo_Project/src/demo/Basics.java
@@ -1,5 +1,10 @@
 package demo;
 
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+
+
+
 public class Basics {
 
 	public static void main(String[] args) 
@@ -7,7 +12,25 @@ public class Basics {
 		// TODO Auto-generated method stub
 		System.out.println("hi");
 		//Comment
-
+		RestAssured.baseURI="https://rahulshettyacademy.com";
+		given().log().all().queryParam("key", "https://rahulshettyacademy.com")
+      .header("Content-Type","application/json").body("{\r\n" + 
+      		"  \"location\": {\r\n" + 
+      		"    \"lat\": -38.383494,\r\n" + 
+      		"    \"lng\": 33.427362\r\n" + 
+      		"  },\r\n" + 
+      		"  \"accuracy\": 50,\r\n" + 
+      		"  \"name\": \"sudhanshu\",\r\n" + 
+      		"  \"phone_number\": \"(+91) 983 893 3937\",\r\n" + 
+      		"  \"address\": \"29, side layout, cohen 09\",\r\n" + 
+      		"  \"types\": [\r\n" + 
+      		"    \"shoe park\",\r\n" + 
+      		"    \"shop\"\r\n" + 
+      		"  ],\r\n" + 
+      		"  \"website\": \"http://google.com\",\r\n" + 
+      		"  \"language\": \"French-IN\"\r\n" + 
+      		"}")
+      .when().post("/maps/api/place/add/json").then().log().all().assertThat().statusCode(200);
 	}
 
 }
