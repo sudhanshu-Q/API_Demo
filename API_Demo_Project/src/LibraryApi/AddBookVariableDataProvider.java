a13faed97069c0e6b1fb84569c911373e9f623cc
sudhanshu <sudhanshu@sudhanshu>
2020-10-17 23:00:32 +0530

    Commit for @data provider

---
+++ API_Demo_Project/src/LibraryApi/AddBookVariableDataProvider.java
@@ -0,0 +1,29 @@
+package LibraryApi;
+
+import static io.restassured.RestAssured.given;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
+public class AddBookVariableDataProvider 
+{
+	@Test(dataProvider="BooksData")
+	//creating a method to pass variable in payload
+	public void addBook(String isbn,String aisle)
+	{
+		RestAssured.baseURI="http://216.10.245.166";
+		String resp=given().header("Content-Type","application/json").body(Payload.AddbookVariablePayload(isbn,aisle)).
+				when().post("/Library/Addbook.php").then().assertThat().statusCode(200).extract().response().asString();
+		JsonPath jsonResp=new JsonPath(resp);
+		String id=jsonResp.get("ID");
+		System.out.println(id);
+	}
+	@DataProvider(name="BooksData")
+	public Object[][] getData()
+	{
+		return new Object[][] {{"fg","23"},{"dasfd","855"},{"dadadc","33"}};
+	}
+	}
--- API_Demo_Project/src/LibraryApi/AddBookVarible.java
+++ API_Demo_Project/src/LibraryApi/AddBookVarible.java
@@ -8,14 +8,14 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 
-public class AddBookVaribl 
+public class AddBookVarible 
 {
 	@Test
 	//creating a method to pass variable in payload
 	public void addBook()
 	{
 		RestAssured.baseURI="http://216.10.245.166";
-		String resp=given().header("Content-Type","application/json").body(Payload.AddbookVariablePayload("isbn","aisle")).
+		String resp=given().header("Content-Type","application/json").body(Payload.AddbookVariablePayload("iasdsasbn","aisle")).
 				when().post("/Library/Addbook.php").then().assertThat().statusCode(200).extract().response().asString();
 		JsonPath jsonResp=new JsonPath(resp);
 		String id=jsonResp.get("ID");
